db1b91df3189dcf6ea0ee383033de9254e91b7a6
artnr7 <artnr7@gmail.com>
2025-08-25 20:18:38 +0300

    parser

--- src/object_class/object_class.cc
+++ src/object_class/object_class.cc
@@ -7,7 +7,35 @@ void s21::Object::ObjectParser() {
 
   std::string obj_file_line = "";
 
-  std::getline(obj_file_stream, obj_file_line);
+  poly_pc_i_t vert_i = 0;
+  while (std::getline(obj_file_stream, obj_file_line)) {
+    ParseVLine(obj_file_line, vert_i);
+  }
+}
 
-  
+void s21::Object::ParseVLine(std::string &obj_file_line, poly_pc_i_t &vert_i) {
+  str_it_t ofl_it = obj_file_line.begin();
+
+  if (*ofl_it != 'v') {
+    return;
+  }
+  vert_it_t vert_it = vertices_.begin();
+  vert_it++->i = vert_i++;
+
+  ParseVLineNums(vert_it, ofl_it);
+}
+
+void s21::Object::ParseVLineNums(vert_it_t &vert_it, str_it_t &ofl_it) {
+  ParseNum(vert_it->x, ofl_it);
+  ParseNum(vert_it->y, ofl_it);
+  ParseNum(vert_it->z, ofl_it);
+}
+
+void s21::Object::ParseNum(coord_t &coord, str_it_t &ofl_it) {
+  std::string num{};
+  while (*ofl_it != ' ' && *ofl_it != '\n') {
+    num += *ofl_it++;
+  }
+  char **endptr{};
+  coord = std::strtold(num.c_str(), endptr);
 }
--- src/object_class/object_class.hpp
+++ src/object_class/object_class.hpp
@@ -1,20 +1,49 @@
+#ifndef OBJECT_HPP_
+#define OBJECT_HPP_
 #include "../model.hpp"
+#include <array>
+#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
 namespace s21 {
+using coord_t = long double;
+using poly_pc_i_t = long int;
+
+typedef struct {
+  poly_pc_i_t i;
+  coord_t x;
+  coord_t y;
+  coord_t z;
+} Vertice;
+
+typedef
+
+using str_it_t = std::string::iterator;
+using vert_it_t = std::vector<s21::Vertice>::iterator;
+
 class Object {
 private:
-  std::vector<long double> peaks_;
+  std::vector<s21::Vertice> vertices_;
   std::vector<long double> surface_;
   std::string file_name_;
 
   void ObjectParser();
+
+
+  // VLine
+  void ParseVLine(std::string &obj_file_line, poly_pc_i_t &peaks_i);
+  void ParseVLineNums(vert_it_t &vert_it, str_it_t &ofl_it);
+  void ParseNum(coord_t &coord, str_it_t &ofl_it);
+  // FLine
+  void ParseFLine()
 
 public:
   Object() = default;
   Object(std::string &file_name);
 };
 } // namespace s21
+
+#endif
