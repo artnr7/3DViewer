6b3aa8f2e416f9f4e269361b91dfe7b50f31f7b0
artnr7 <artnr7@gmail.com>
2025-08-26 19:58:18 +0300

    parser f

--- src/object_class/object_class.cc
+++ src/object_class/object_class.cc
@@ -1,26 +1,60 @@
 #include "object_class.hpp"
 
-s21::Object::Object(std::string &file_name) : file_name_(file_name) {}
+s21::Object::Object(std::string &file_name)
+    : vertices_(), faces_(), file_name_(file_name) {}
 
 void s21::Object::ObjectParser() {
   std::ifstream obj_file_stream(file_name_);
 
   std::string obj_file_line = "";
 
-  poly_pc_i_t vert_i = 0;
+  poly_pc_i_t vert_i = 0, face_i = 0;
+
   while (std::getline(obj_file_stream, obj_file_line)) {
     ParseVLine(obj_file_line, vert_i);
+    ParseFLine(obj_file_line, face_i);
   }
 }
+
+void s21::Object::ParseFLine(std::string &obj_file_line, poly_pc_i_t &face_i) {
+  str_it_t ofl_it = obj_file_line.begin();
+
+  if (*ofl_it++ != 'f') {
+    return;
+  }
+
+  ParseFMap(face_i, ofl_it);
+}
+
+void s21::Object::ParseFMap(poly_pc_i_t &face_i, str_it_t &ofl_it) {
+  std::string num{};
+  while (*ofl_it != ' ' && *ofl_it != '\n') {
+    num += *ofl_it++;
+  }
+  char **endptr{};
+  faces_[face_i].map = std::strtold(num.c_str(), endptr);
+
+
+
+  faces_.push_back({});
+  while (*ofl_it != '\n') {
+    faces_[face_i].i = face_i;
+    faces_[face_i].map = {};
+
+    ParseFMapEls(face_i, ofl_it);
+  }
+}
+
+void s21::Object::ParseFMapEls(poly_pc_i_t &face_i, str_it_t &ofl_it) {}
 
 void s21::Object::ParseVLine(std::string &obj_file_line, poly_pc_i_t &vert_i) {
   str_it_t ofl_it = obj_file_line.begin();
 
-  if (*ofl_it != 'v') {
+  if (*ofl_it++ != 'v') {
     return;
   }
-  vert_it_t vert_it = vertices_.begin();
-  vert_it++->i = vert_i++;
+  vert_it_t vert_it = vertices_.begin() + vert_i;
+  vert_it->i = vert_i++;
 
   ParseVLineNums(vert_it, ofl_it);
 }
--- src/object_class/object_class.hpp
+++ src/object_class/object_class.hpp
@@ -19,26 +19,35 @@ typedef struct {
   coord_t z;
 } Vertice;
 
-typedef
+typedef struct {
+  typedef struct {
+    Vertice vertice;
+  } MapEl;
+
+  poly_pc_i_t i;
+  std::vector<FaceEl> map;
+} Face;
 
 using str_it_t = std::string::iterator;
 using vert_it_t = std::vector<s21::Vertice>::iterator;
+using face_it_t = std::vector<s21::Face>::iterator;
 
 class Object {
 private:
   std::vector<s21::Vertice> vertices_;
-  std::vector<long double> surface_;
+  std::vector<s21::Face> faces_;
   std::string file_name_;
 
   void ObjectParser();
-
 
   // VLine
   void ParseVLine(std::string &obj_file_line, poly_pc_i_t &peaks_i);
   void ParseVLineNums(vert_it_t &vert_it, str_it_t &ofl_it);
   void ParseNum(coord_t &coord, str_it_t &ofl_it);
   // FLine
-  void ParseFLine()
+  void ParseFLine(std::string &obj_file_line, poly_pc_i_t &face_i);
+  void ParseFMap(poly_pc_i_t &face_i, str_it_t &ofl_it);
+  void ParseFMapEls(poly_pc_i_t &face_i, str_it_t &ofl_it);
 
 public:
   Object() = default;
