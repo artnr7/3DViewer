7499d4a45528e2ae1651b720036ca11fefee406b
artnr7 <artnr7@gmail.com>
2025-09-25 19:42:54 +0300

    checkout init

--- src/app/view/main_window/main_window.hpp
+++ src/app/view/main_window/main_window.hpp
@@ -2,15 +2,17 @@
 #define MAIN_WINDOW_HPP_
 
 #include <QWidget>
-// #include "menu_widget/menu_widget.hpp"
+
+#include "menu_widget/menu_widget.hpp"
 #include "object_viewer_widget/object_viewer_widget.hpp"
+
 
 namespace s21 {
 class MainWindow : public QWidget {
   Q_OBJECT
  private:
   ObjectViewerWidget* p_obj_v_wid_;
-  // std::unique_ptr<MenuWidget> p_menu_wid_;
+  MenuWidget* p_menu_wid_;
 
  public:
   MainWindow();
