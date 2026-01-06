ed79d6075f2b248aa333a836cdbf2e7fa6523784
Ildarc4S <ildarc4S@gmail.com>
2026-01-07 01:10:10 +0300

    Fix main CMakeLists. Issue #3

---
+++ src/cmake/TargetLinkFunctions.cmake
@@ -0,0 +1,17 @@
+function(target_link_gtest TARGET_NAME)
+  target_link_libraries(${TARGET_NAME} PRIVATE
+    GTest::gtest
+    GTest::gtest_main
+    Threads::Threads
+  )
+endfunction()
+
+function(target_link_qt TARGET_NAME)
+  target_link_libraries(${TARGET_NAME} PRIVATE
+    Qt6::Core
+    Qt6::Widgets
+    Qt6::OpenGLWidgets
+    Qt6::OpenGL
+    Qt6::Gui
+  )
+endfunction()
