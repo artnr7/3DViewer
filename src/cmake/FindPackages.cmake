72f477041c689a35c67e3d659891a878d9465cfc
Ildarc4S <ildarc4S@gmail.com>
2026-01-07 01:08:47 +0300

    Add new find packages

--- src/cmake/FindPackages.cmake
+++ src/cmake/FindPackages.cmake
@@ -5,3 +5,10 @@ find_package(Qt6 REQUIRED COMPONENTS Core Widgets OpenGL OpenGLWidgets Gui)
 # Find GTest
 find_package(GTest REQUIRED)
 find_package(Threads REQUIRED)
+
+# Find Lcov and GenHtml
+find_program(LCOV lcov)
+find_program(GENHTML genhtml)
+
+# Find Clang Format
+find_program(CLANG_FORMAT "clang-format")
